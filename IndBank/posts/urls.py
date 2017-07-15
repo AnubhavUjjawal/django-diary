15bd62fecb295a9e7b103fb7f74a1f594da9b9dd
Anubhav Ujjawal <anubhav.u16@iiits.in>
2017-07-15 19:34:11 +0530

    Update urls.py

--- IndBank/posts/urls.py
+++ IndBank/posts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^signup$',add_user,name='add_user'),
     url(r'^logout$',user_logout,name='user_logout'),
     url(r'^home/(?P<username>[0-9A-Za-z\.@\-]+)$',homepage,name='homepage'),
+    url(r'^profile/(?P<username>[0-9A-Za-z\.@\-]+)$',profile,name='profile'),    
     url(r'^login',user_login,name='user_login'),
     url(r'^$',login_page,name='login_page'),
     url(r'^submit$',add_post,name="submit_post")
